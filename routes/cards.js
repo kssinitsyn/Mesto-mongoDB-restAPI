494d90d819a26adc40ef434cc68435977d558f3e
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2019-10-22 11:32:52 +0300

    add MongoDB, routes, controllers

---
+++ routes/cards.js
@@ -0,0 +1,8 @@
+const cards = require('express').Router();
+const { createCard, getAllCards, deleteCard } = require('../controllers/cards')
+
+cards.get('/', getAllCards);
+cards.post('/', createCard);
+cards.delete('/:id', deleteCard);
+
+module.exports = cards;
